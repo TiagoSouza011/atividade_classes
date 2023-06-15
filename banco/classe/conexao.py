b50661f29e04b7ec49a2220060abb2dd7a999698
TiagoSouza011 <124839103+TiagoSouza011@users.noreply.github.com>
2023-06-14 22:28:23 -0300

    atividade_classes

--- banco/classe/conexao.py
+++ banco/classe/conexao.py
@@ -3,8 +3,8 @@ import mysql.connector
 def conectar():
     mydb = mysql.connector.connect(
         host="localhost",
-        user="admin",
-        password="tiago1234",
-        database="database15"
+        user="root",
+        password="r00t",
+        database="atividade_classes"
     )
     return mydb
